246b6cc2261cdbe186a5c24587b456f968b8a069
Igor Bari <mucsi96@gmail.com>
2024-03-14 08:34:41 +0000

    consume valut_key as CLI argument

--- scripts/build_test_image.py
+++ scripts/build_test_image.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+from os import makedirs
+from posixpath import dirname
 import init
 import sys
 from pathlib import Path
@@ -7,12 +9,21 @@ from src.ansible_utils import load_vars
 from src.docker_utils import build_and_push_docker_img
 
 root_directory = Path(__file__).parent.parent
-data = load_vars(root_directory / '.ansible/vault_key',
+access_token = sys.argv[1]
+vault_key = sys.argv[2]
+vault_key_file = root_directory / '.ansible/vault_key'
+
+makedirs(dirname(vault_key_file))
+with open(vault_key_file, 'w') as file:
+    file.write(vault_key)
+
+print(len(vault_key))
+
+data = load_vars(vault_key_file,
                  root_directory / 'vars/vault.yaml')
 docker_username = data['docker_username']
 docker_password = data['docker_password']
 
-access_token = sys.argv[1]
 
 if not access_token:
     print("GitHub access token is missing", flush=True, file=sys.stderr)
