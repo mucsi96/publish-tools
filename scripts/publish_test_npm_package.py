2afb5cca2d2948915a486196d452465bcbc7feb2
Igor Bari <mucsi96@gmail.com>
2024-03-17 17:52:50 +0000

    add npm package publishing

---
+++ scripts/publish_test_npm_package.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+from os import environ, makedirs
+from posixpath import dirname
+from tempfile import NamedTemporaryFile
+import init
+import sys
+from pathlib import Path
+from src.ansible_utils import load_vars
+from src.npm_utils import publish_npm_package
+
+root_directory = Path(__file__).parent.parent
+secrets = load_vars(sys.argv[2], root_directory / 'vars/vault.yaml')
+
+publish_npm_package(
+    src="src",
+    tag_prefix="npm-package",
+    npm_access_token=secrets['npm_access_token'],
+    github_access_token=sys.argv[1]
+)
---
+++ src/npm_utils.py
@@ -0,0 +1,59 @@
+import json
+import sys
+
+from subprocess import run
+from pathlib import Path
+from textwrap import dedent
+from typing import List
+from .github_utils import create_release
+from .version_utils import get_version
+
+def get_npm_package_name(package_json_path: Path):
+    with open(package_json_path, 'r') as file:
+        package_data = json.load(file)
+        return package_data['name']
+
+def publish_npm_package(
+    *,
+    src: Path,
+    tag_prefix: str,
+    npm_access_token: str,
+    github_access_token: str,
+    ignore: List[str] = [],
+):
+    package_name = get_npm_package_name(src)
+    
+    if not npm_access_token:
+        print('NPM access token is missing', flush=True, file=sys.stderr)
+        exit(1)
+
+    if not github_access_token:
+        print('GitHub access token is missing', flush=True, file=sys.stderr)
+        exit(1)
+
+    changed, version = get_version(
+        src=src, tag_prefix=tag_prefix, ignore=ignore)
+
+    if not changed:
+        return
+    
+    with open(src / '.npmrc', 'w') as file:
+        file.write(f'registry=https://registry.npmjs.org')
+
+    run(['npm', 'publish'], env={
+        'NODE_AUTH_TOKEN': npm_access_token}, check=True)
+
+    create_release(
+        tag_prefix=tag_prefix,
+        version=version,
+        access_token=github_access_token,
+        body=dedent(f'''
+            [NPM package](https://www.npmjs.com/package/{package_name})
+
+            ```json
+            "{package_name}": "{version}"
+            ```
+        ''')
+    )
+    print(
+        f'NPM package pushed successfully for {tag_prefix}:{version}', flush=True)
