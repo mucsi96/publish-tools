a1909b4ab61a3ed8624f41b8e240eab922c717b3
Igor Bari <mucsi96@gmail.com>
2024-03-20 18:17:18 +0000

    try underscore

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from os import getenv
 from setuptools import setup
 
 setup(
-    name='publish-tools',
+    name='publish_tools',
     version=f'{getenv("LIB_VERSION")}.0.0',
     author="Igor Bari",
-    package_dir={"publish-tools": "src"}
+    package_dir={"publish_tools": "src"}
 )
