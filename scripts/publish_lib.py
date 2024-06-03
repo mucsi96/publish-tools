2dc0ba18b6afc1136de6b5974f468c74bee681f9
Igor Bari <mucsi96@gmail.com>
2024-06-03 11:13:25 +0200

    publish to pypi

--- scripts/publish_lib.py
+++ scripts/publish_lib.py
@@ -4,6 +4,7 @@
 from pathlib import Path
 from shutil import rmtree
 from subprocess import run
+from textwrap import dedent
 import toml
 import init
 import sys
@@ -15,12 +16,12 @@ root_directory = Path(__file__).parent.parent
 
 
 def set_package_version(version: int):
-    with open('pyproject.toml', 'r') as file:
+    with open("pyproject.toml", "r") as file:
         package_data = toml.load(file)
 
-    package_data['project']['version'] = f'{version}.0.0'
+    package_data["project"]["version"] = f"{version}.0.0"
 
-    with open('pyproject.toml', 'w') as file:
+    with open("pyproject.toml", "w") as file:
         toml.dump(package_data, file)
 
 
@@ -30,7 +31,7 @@ if not access_token:
     print("GitHub access token is missing", flush=True, file=sys.stderr)
     exit(1)
 
-version = get_version(src=root_directory, tag_prefix="version")
+version = 21
 
 rmtree("build", ignore_errors=True)
 rmtree("dist", ignore_errors=True)
@@ -38,15 +39,25 @@ rmtree("publish_tools.egg-info", ignore_errors=True)
 
 set_package_version(version)
 
-ProjectBuilder(source_dir='.').build(
-    distribution='wheel', output_directory='dist')
+ProjectBuilder(source_dir=".").build(distribution="wheel", output_directory="dist")
 
-run(['unzip', '-l', 'dist/*.whl'])
+run(["unzip", "-l", "dist/*.whl"])
+run(["twine", "check", "dist/*.whl"])
+run(["twine", "upload", "dist/*.whl", "--non-interactive", "--verbose"])
 
-release_id = create_release(
-    version=version, access_token=access_token, tag_prefix="version")
-upload_release_asset(
-    release_id=release_id,
-    filename_pattern="dist/publish_tools-*.whl",
+package_name = "mucsi96.publish-tools"
+
+create_release(
+    version=version,
     access_token=access_token,
+    tag_prefix="version",
+    body=dedent(
+        f"""
+            [PyPI package](https://pypi.org/project/{package_name})
+
+            ```toml
+            {package_name} == {version}.0.0
+            ```
+        """
+    ),
 )
